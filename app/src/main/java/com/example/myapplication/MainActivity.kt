243741444f848c6c7f2bad46adb9bf98efa4ccf4
mikha <mixa199525@yandex.ru>
2023-04-03 22:30:56 +0300

    TextAndButton

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var tvText: TextView
+    lateinit var btnButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
+        tvText = findViewById(R.id.tvText)
+        btnButton = findViewById(R.id.btnTest)
+
+        btnButton.setOnClickListener {
+            tvText.text = "it is work"
+        }
     }
 }
